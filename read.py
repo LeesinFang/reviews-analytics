c1199ca9334c26d67fed80ef7aa51045ddcaeea2
Leesin <a0985030457@gmail.com>
2024-10-03 00:28:08 +0800

    add new function

--- read.py
+++ read.py
@@ -15,3 +15,12 @@ for w in data:
 	sum_len += len(w)
 print('留言的平均長度為', sum_len/len(data))
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+
+
+
