d900970d30e6c9414abf061a145322854c1de3eb
Leesin <a0985030457@gmail.com>
2024-10-03 00:21:01 +0800

    clac avg len of reviews

--- read.py
+++ read.py
@@ -1,14 +1,17 @@
 data = []
 count = 0
 with open('reviews.txt', 'r')as f:
-	for line in f:
+	for line in f: #逐一讀取檔案中的每一行，將其存放在變數 line 中
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
 print(len(data))
 
-print(data[0])
-print('----------------')
-print(data[1])
+print(f'檔案讀取完了,總共有{len(data)}筆資料')
+
+sum_len = 0
+for w in data:
+	sum_len += len(w)
+print('留言的平均長度為', sum_len/len(data))
 
